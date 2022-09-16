afc8601419579b0add706a11bedc82569046a58f
Michael <mrmchughes27@gmail.com>
2022-09-16 14:23:44 -0400

    verified API can connect to Front End

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
 import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  fetch(
+    "https://rest-api-for-blog-production.up.railway.app/posts/630fe7b526743c6b1ea7d1bf/comments",
+    {
+      mode: "cors",
+    }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      console.log(response);
+    });
+  return <div>Different ways to fetch posts</div>;
 }
 
 export default App;

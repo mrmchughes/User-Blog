8c0d9977a8b3530c172d5edf280db9571c13b1d0
Michael <mrmchughes27@gmail.com>
2022-09-19 15:22:54 -0400

    created initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CommentBox from "./components/CommentBox";
+import HomePage from "./components/HomePage";
+import NavBar from "./components/NavBar";
+import PostCard from "./components/PostCard";
+import PostPage from "./components/PostPage";
+import "./styles/global.css";
 
 function App() {
-  fetch(
-    "https://rest-api-for-blog-production.up.railway.app/posts/630fe7b526743c6b1ea7d1bf/comments",
-    {
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  interface Post {
+    _id: string;
+    isPublished: boolean;
+    title: string;
+    user: string;
+    timestamp: string;
+    message: string;
+  }
+
+  useEffect(() => {
+    fetch("https://rest-api-for-blog-production.up.railway.app", {
       mode: "cors",
-    }
-  )
-    .then(function (response) {
-      return response.json();
     })
-    .then(function (response) {
-      console.log(response);
-    });
-  return <div>Different ways to fetch posts</div>;
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        setPosts(response);
+      });
+  }, [posts]);
+
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage posts={posts} />} />
+        <Route path="*" element={<p> There is nothing here!</p>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

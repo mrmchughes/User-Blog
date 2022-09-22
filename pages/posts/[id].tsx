1df201ff3db715072b68bb112b2f98202aa776d6
Michael <mrmchughes27@gmail.com>
2022-09-22 13:07:26 -0400

    created fresh app, wiped tutorial files

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,41 +1,13 @@
 import Layout from "../../components/layout";
-import { getAllPostIds, getPostData } from "../../lib/posts";
-import Date from "../../components/date";
-import Head from "next/head";
-import utilStyles from "../../styles/utils.module.css";
+import Link from "next/link";
 
-export default function Post({ postData }) {
+export default function Post() {
   return (
     <Layout>
-      <Head>
-        <title>{postData.title}</title>
-      </Head>
-
-      <article>
-        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
-        <div className={utilStyles.lightText}>
-          <Date dateString={postData.date} />
-        </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      </article>
+      <h1>{post.title}</h1>
+      <h2>
+        <Link href="/">Back to Home!</Link>
+      </h2>
     </Layout>
   );
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
 }

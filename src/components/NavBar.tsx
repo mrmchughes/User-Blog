db3b068bcc5c23e1827911153d7db2c521171c66
Michael <mrmchughes27@gmail.com>
2022-09-30 15:07:17 -0400

    worked on NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 
 const NavBar = () => {
   return (
-    <div>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            Home
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 

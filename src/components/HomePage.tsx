72fead996896d9b77d8b2018d4b62d1a686c3693
Michael <mrmchughes27@gmail.com>
2022-10-12 17:27:29 -0400

    worked on hero section

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,6 +2,20 @@ import React from "react";
 import PostCard from "../components/PostCard";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Unstable_Grid2";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+
+const theme = createTheme();
+
+theme.typography.h3 = {
+  fontSize: "1.2rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.5rem",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2rem",
+  },
+};
 
 interface Post {
   _id: string;
@@ -19,20 +33,29 @@ interface HomePageProps {
 const HomePage = ({ posts }: HomePageProps) => {
   return (
     <Box component="main">
-      <div>
-        <p>I'm Michael, welcome to my Blog!</p>
-        <p>
-          Here is some more filler text that explains what my blog is all about.
-        </p>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Box sx={{ boxShadow: 3, width: "100%", mb: 5, p: 5 }}>
+          <Typography
+            variant="h3"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", sm: "block" },
+              textAlign: "center",
+            }}
+          >
+            Hello World! I'm Michael, and welcome to my Blog!
+          </Typography>
+        </Box>
+      </ThemeProvider>
 
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
+      <Box sx={{ flexGrow: 1, m: 5 }}>
+        <Grid container spacing={3}>
           {posts.map((post) => (
             <Grid
               xs={12}
               sm={6}
-              md={3}
+              md={4}
               key={post._id}
               display="flex"
               justifyContent="center"
